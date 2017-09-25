f6a7b32f3d3488760af6319d49cf7905a8ecfe16
yihongbo123 <30612761+yihongbo123@users.noreply.github.com>
2017-09-25 14:57:40 +0800

    Initial commit

---
+++ microservice-hystrix-turbine/src/main/java/com/itmuch/cloud/study/TurbineApplication.java
@@ -0,0 +1,17 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * 通过@EnableTurbine接⼝，激活对Turbine的⽀持。
+ * @author eacdy
+ */
+@SpringBootApplication
+@EnableTurbine
+public class TurbineApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(TurbineApplication.class).web(true).run(args);
+    }
+}
